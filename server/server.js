2486ab6d3800d07b43ecbeb58b36bd7b35d91d6d
Mattias Söderberg <mattes.email@gmail.com>
2022-04-13 10:13:44 +0200

    Add dotenv

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
+const dotenv = require("dotenv")
+dotenv.config()
 const app = require("./app")
 const mongoose = require("mongoose")
 
-const PORT = 5000;
-const MONGODB_URL = "mongodb://127.0.0.1/ecommerce"
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL
 
 mongoose.connect(MONGODB_URL)
 
 app.listen(PORT, () => {
-    console.log(`Started Express server on port ${PORT}`);
-  });
+  console.log(`Started Express server on port ${PORT}`);
+});
